fec620d6df9d33d317bee2570173539a698f3899
Bhavik Jikadara <66177876+Bhavik-Jikadara@users.noreply.github.com>
2024-07-21 11:27:45 +0530

    Initial release

---
+++ README.md
@@ -0,0 +1,144 @@
+# AI/ML Roadmap for Beginners in 2024
+
+Welcome to the ultimate AI/ML roadmap for 2024! This guide is designed to help you navigate the complex world of artificial intelligence and machine learning, offering a step-by-step approach to mastering these technologies.
+
+## 1. Fundamentals of Programming
+
+Start with learning the basics of programming. Familiarize yourself with languages such as Python, which is widely used in AI/ML. Key topics include:
+
+- Variables and Data Types
+- Control Structures (if-else, loops)
+- Functions and Modules
+- Object-Oriented Programming (OOP)
+- Basic Data Structures (lists, dictionaries, sets)
+
+## 2. Mathematics for AI/ML
+
+Mathematics forms the foundation of AI/ML. Focus on the following areas:
+
+- Linear Algebra (vectors, matrices, eigenvalues)
+- Calculus (differentiation, integration)
+- Probability and Statistics (distributions, hypothesis testing)
+- Optimization Techniques
+
+## 3. Basics of AI/ML
+
+Understand the core concepts and terminologies in AI/ML:
+
+- What is AI? What is ML?
+- Supervised vs. Unsupervised Learning
+- Key algorithms: Linear Regression, Decision Trees, K-Nearest Neighbors
+- Overfitting and Underfitting
+- Evaluation Metrics (accuracy, precision, recall, F1-score)
+
+## 4. Data Skills for AI/ML
+
+Learn how to work with data, the backbone of AI/ML:
+
+- Data Collection and Cleaning
+- Exploratory Data Analysis (EDA)
+- Feature Engineering
+- Data Visualization (using libraries like Matplotlib, Seaborn)
+
+## 5. Machine Learning
+
+Dive deeper into machine learning:
+
+- Advanced Algorithms: SVM, Random Forests, Gradient Boosting
+- Ensemble Learning
+- Model Evaluation and Validation
+- Hyperparameter Tuning
+- Introduction to ML Frameworks (Scikit-learn, TensorFlow, PyTorch)
+
+## 6. Deep Learning
+
+Explore the world of deep learning:
+
+- Neural Networks and Backpropagation
+- Deep Learning Architectures (CNNs, RNNs)
+- Training Deep Networks
+- Transfer Learning
+- Frameworks: TensorFlow, Keras, PyTorch
+
+## 7. Natural Language Processing
+
+Specialize in processing and analyzing text data:
+
+- Text Preprocessing
+- Sentiment Analysis
+- Named Entity Recognition (NER)
+- Language Models (BERT, GPT)
+- Chatbots and Conversational AI
+
+## 8. Computer Vision
+
+Focus on techniques for processing and understanding images:
+
+- Image Preprocessing
+- Convolutional Neural Networks (CNNs)
+- Object Detection and Segmentation
+- Image Generation (GANs)
+- Applications in Healthcare, Automotive, etc.
+
+## 9. Reinforcement Learning
+
+Learn about agents and environments:
+
+- Markov Decision Processes (MDP)
+- Q-Learning and Deep Q-Networks (DQN)
+- Policy Gradient Methods
+- Applications in Game AI, Robotics
+
+## 10. Tools and Libraries
+
+Familiarize yourself with essential tools and libraries:
+
+- Jupyter Notebooks
+- Scikit-learn
+- TensorFlow and Keras
+- PyTorch
+- Pandas and Numpy
+
+## 11. Build AI/ML Applications
+
+Apply your knowledge to build real-world applications:
+
+- End-to-end Machine Learning Projects
+- Deployment of Models (using Flask, Docker)
+- Model Monitoring and Maintenance
+- Case Studies and Examples
+
+## 12. Knowledge on Recent Trends and Advancements
+
+Stay updated with the latest in AI/ML:
+
+- Read Research Papers
+- Follow AI/ML Blogs and News
+- Participate in Competitions (Kaggle, DrivenData)
+- Join AI/ML Communities and Meetups
+
+## 13. The Super Duper NLP Repo
+
+Check out the "Super Duper NLP Repo" for a comprehensive collection of NLP resources and projects.
+
+## Follow
+
+Connect with me on various platforms:
+
+- [LinkedIn](https://www.linkedin.com/in/bhavikjikadara)
+- [GitHub](https://github.com/Bhavik-Jikadara)
+- [Facebook](https://www.facebook.com/Bhavikjikadara07)
+- [Instagram](https://www.instagram.com/bhavikjikadara/)
+- [Twitter](https://twitter.com/BhavikJikadara1)
+
+## Subscribe
+
+Stay tuned for more content by subscribing to my YouTube channel: [YouTube](https://www.youtube.com/channel/UC7Bp_sYQmAryrrPqvUp6PwQ)
+
+## Donate & Support Us
+
+If you find this guide helpful, consider supporting us through donations: [PayPal](https://www.paypal.com/paypalme/bhavikjikadara)
+
+---
+
+### Feel free to explore each section, and don't hesitate to reach out if you have any questions or need further guidance. Happy learning
---
+++ src/App.jsx
@@ -0,0 +1,222 @@
+import './App.css'
+
+function App() {
+    return (
+        <>
+            <div className="title">
+                <h1>AI/ML Roadmap for Beginners in 2024</h1>
+                <p>Welcome to the ultimate AI/ML roadmap for 2024! This guide is designed to help you navigate the complex world of artificial intelligence and machine learning, offering a step-by-step approach to mastering these technologies.
+                </p>
+            </div>
+
+            <hr></hr>
+
+            <div className="timeline">
+                <div className="checkpoint">
+                    <div className="sub-checkpoint">
+                        <h2>Funamentals of Programming</h2>
+                        <ul>
+                            <li><a href="https://youtu.be/rfscVS0vtbw?si=-g8pxFjh634NP2ik" target="_blank">Python
+                                Programming</a></li>
+                            <li><a href="https://youtu.be/_V8eKsto3Ug?si=z6ze4xp02cF6REzV" target="_blank">R Programming</a>
+                            </li>
+                            <li><a href="https://youtu.be/2ZLl8GAk1X4?si=RRdusoShLTKGhRY2">Algorithms and Data
+                                Structures</a>
+                            </li>
+                            <li><a href="https://youtu.be/oBt53YbR9Kk?si=4Iz7YHrwKgvPxsk-">Problem Solving Techniques</a>
+                            </li>
+                            <li><a href="https://youtu.be/Ej_02ICOIgs?si=F8aiU6NJDwM4BM2X">OOP Concepts</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="checkpoint">
+                    <div className="sub-checkpoint">
+                        <h2>Mathematics for AI/ML</h2>
+                        <ul>
+                            <li><a href="https://math.mit.edu/~djk/calculus_beginners/" target="_blank">Calculus</a></li>
+                            <li><a href="https://immersivemath.com/ila/learnmore.html" target="_blank">Linear Algebra</a>
+                            </li>
+                            <li><a href="https://www.khanacademy.org/math/statistics-probability" target="_blank">Probability
+                                and Statistics</a></li>
+                            <li><a href="https://www.khanacademy.org/math/differential-equations" target="_blank">Differential
+                                Equations</a></li>
+                            <li><a href="https://youtube.com/playlist?list=PLHXZ9OQGMqxersk8fUxiUMSIx0DBqsKZS&si=nh4dvkLqD2pLsNh-"
+                                target="_blank">Discrete Mathematics</a></li>
+                            <li><a href="https://www.youtube.com/playlist?list=PLLtQL9wSL16ioUvHckGCkoWq_CIvyUI0p"
+                                target="_blank">Optimization Technologies</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="checkpoint">
+                    <div className="sub-checkpoint">
+                        <h2>Basics of AI/ML</h2>
+                        <ul>
+                            <li><a href="https://youtu.be/ukzFI9rgwfU?si=j0XZ_rPJkxotPdDI" target="_blank">Introductions to
+                                Supervised and Unsupervised Learning in Machine Learning, Neural Networks, Deep
+                                Learning,
+                                and Reinforcement Learning</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="checkpoint">
+                    <div className="sub-checkpoint">
+                        <h2>Data Skills for AI/ML</h2>
+                        <ul>
+                            <li><a href="https://labelyourdata.com/articles/data-collection-methods-AI">Data Collection</a>
+                            </li>
+                            <li><a href="https://monkeylearn.com/blog/data-cleaning-steps/">Data Cleaning and Processing</a>
+                            </li>
+                            <li><a
+                                href="https://builtin.com/articles/feature-engineering#:~:text=Apr%2029%2C%202024-,Feature%20engineering%20is%20the%20process%20of%20selecting%2C%20manipulating%20and%20transforming,used%20in%20a%20predictive%20model.">Feature
+                                Engineering</a></li>
+                            <li><a href="https://towardsdatascience.com/exploratory-data-analysis-8fc1cb20fd15">Exploratory
+                                Data
+                                Analysis</a></li>
+                            <li><a href="https://www.datacamp.com/blog/data-visualization-techniques">Data Visualization
+                                Technologies</a></li>
+                            <li><a href="https://www.kaggle.com/discussions/getting-started/251992">Use of Libraries like
+                                Pandas, Numpy Matplotlib, Seaborn</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="checkpoint">
+                    <div className="sub-checkpoint">
+                        <h2>Machine Learning</h2>
+                        <ul>
+                            <li><a href="https://www.youtube.com/watch?v=JxgmHe2NyeY&t=85s">Linear Regression, Logistic
+                                Regression</a></li>
+                            <li><a href="https://www.youtube.com/watch?v=JxgmHe2NyeY&t=85s">Decision Trees and Random
+                                Forests</a></li>
+                            <li><a href="https://www.youtube.com/watch?v=JxgmHe2NyeY&t=85s">Support Vector Machines</a></li>
+                            <li><a href="https://www.youtube.com/watch?v=JxgmHe2NyeY&t=85s">K-Nearest Neighbours</a></li>
+                            <li><a href="https://www.youtube.com/watch?v=JxgmHe2NyeY&t=85s">Naive Bayes</a></li>
+                            <li><a href="https://www.youtube.com/watch?v=JxgmHe2NyeY&t=85s">XGBoost, LightGBM, CatBoost</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="checkpoint">
+                    <div className="sub-checkpoint">
+                        <h2>Deep Learning</h2>
+                        <ul>
+                            <li><a href="https://www.youtube.com/watch?v=d2kxUVwWWwU">ANN and CNN</a></li>
+                            <li><a href="https://www.theaidream.com/post/introduction-to-rnn-and-lstm">RNN and LSTM</a></li>
+                            <li><a
+                                href="https://machinelearningmastery.com/what-are-generative-adversarial-networks-gans/">GANs</a>
+                            </li>
+                            <li><a href="https://blogs.nvidia.com/blog/what-is-a-transformer-model/">Transformer Models</a>
+                            </li>
+                            <li><a href="https://www.datacamp.com/tutorial/pytorch-vs-tensorflow-vs-keras">Deep Learning
+                                Libraries (Tensorflow, PyTorch, Keras)</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="checkpoint">
+                    <div className="sub-checkpoint">
+                        <h2>Natural Language Processing</h2>
+                        <ul>
+                            <li><a href="https://www.youtube.com/playlist?list=PLZoTAELRMXVNNrHSKv36Lr3_156yCo6Nn">Text
+                                Preprocessing Techniques</a></li>
+                            <li><a href="https://www.youtube.com/playlist?list=PLZoTAELRMXVNNrHSKv36Lr3_156yCo6Nn">Word
+                                Embeddings</a></li>
+                            <li><a href="https://www.youtube.com/playlist?list=PLZoTAELRMXVNNrHSKv36Lr3_156yCo6Nn">Bag of
+                                Words,
+                                TF-IDF</a></li>
+                            <li><a href="https://www.youtube.com/playlist?list=PLZoTAELRMXVNNrHSKv36Lr3_156yCo6Nn">LSTMs,
+                                GRU</a></li>
+                            <li><a href="https://youtu.be/7kLi8u2dJz0?si=uFBZbpPylf_wUxIs">Transformers and BERT</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="checkpoint">
+                    <div className="sub-checkpoint">
+                        <h2>Computer Vision</h2>
+                        <ul>
+                            <li><a href="https://youtu.be/IA3WxTTPXqQ?si=60PIJEvV1XUwSncT">Image Processing Techniques</a>
+                            </li>
+                            <li><a href="https://youtu.be/IA3WxTTPXqQ?si=60PIJEvV1XUwSncT">Convolutional Neural Networks</a>
+                            </li>
+                            <li><a href="https://youtu.be/IA3WxTTPXqQ?si=60PIJEvV1XUwSncT">Object Detection Algorithms</a>
+                            </li>
+                            <li><a href="https://youtu.be/IA3WxTTPXqQ?si=60PIJEvV1XUwSncT">Image Segmentation</a></li>
+                            <li><a href="https://youtu.be/IA3WxTTPXqQ?si=60PIJEvV1XUwSncT">Facial Recognition Techniques</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="checkpoint">
+                    <div className="sub-checkpoint">
+                        <h2>Reinforcement Learning</h2>
+                        <ul>
+                            <li><a href="https://youtu.be/14BfO5lMiuk?si=FBib9Nyj-jLVzbtP">Bellman Equation</a></li>
+                            <li><a href="https://youtu.be/0iqz4tcKN58?si=gzOkPVPXnNZXFMgJ">Q-Learning</a></li>
+                            <li><a href="https://www.geeksforgeeks.org/sarsa-reinforcement-learning/">SARSA
+                                (State-Action-Reward-State-Action)</a></li>
+                            <li><a href="https://www.tensorflow.org/agents/tutorials/0_intro_rl">Deep Q-Network</a></li>
+                            <li><a href="https://towardsdatascience.com/policy-gradients-in-a-nutshell-8b72f9743c5d">Policy
+                                Gradient Methods</a></li>
+                            <li><a
+                                href="https://www.analyticsvidhya.com/blog/2018/11/reinforcement-learning-introduction-monte-carlo-learning-openai-gym/#:~:text=A.,a%20model%20of%20the%20environment.">Monte
+                                Carlo Methods</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="checkpoint">
+                    <div className="sub-checkpoint">
+                        <h2>Tools and Libraries</h2>
+                        <ul>
+                            <li>Scipy, Scikit-Learn</li>
+                            <li>Keras, TensorFlow, PyTorch</li>
+                            <li>OpenCV</li>
+                            <li>Matplotlib, Seaborn, Plotly</li>
+                            <li>Pandas, Numpy</li>
+                            <li>Jupyter Notebook, Jupyter Lab</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="checkpoint">
+                    <div className="sub-checkpoint">
+                        <h2>Build AI/ML Applications</h2>
+                        <ul>
+                            <li>End-to-end Model Development</li>
+                            <li>Application Integration(Web, Mobile)</li>
+                            <li>Using ML Cloud Platforms(AWS, Azure, GCP)</li>
+                            <li>Using AI API Services</li>
+                            <li>Deployment and Scaling of Models</li>
+                            <li>Model Optimization Techniques</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="checkpoint">
+                    <div className="sub-checkpoint">
+                        <h2>Knowledge on Recent Trends and Advacements</h2>
+                        <ul>
+                            <li>Quantum Computing</li>
+                            <li>Federated Learning</li>
+                            <li>AI Ethics and Fairness</li>
+                            <li>AutoML and Neural Architecture Search</li>
+                            <li>Explainable AI</li>
+                            <li>AI in Edge devices</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="checkpoint">
+                    <div className="sub-checkpoint">
+                        <h2>The Super Duper NLP Repo</h2>
+                        <ul>
+                            <li><a href="https://notebooks.quantumstat.com/">The Super Duper NLP Repo</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <footer>
+                <small>
+                    &copy; 2024 AI/ML Roadmap. All rights reserved.
+                </small>
+            </footer>
+        </>
+    )
+}
+
+export default App
